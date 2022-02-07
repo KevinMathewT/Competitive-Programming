dec87615567d6c306c3e6ac2908c33be3b16237b
Anupam Haldkar <ahdev2020@outlook.com>
2022-02-07 16:43:12 +0530

    Update Bashar and the bad land.cpp
    I have remove the condition when left and right are same, it doesn't matter because atleast he has to go either one house to achieve.
    I have successfully submitted the code in the platform as well.

--- Bashar and the bad land.cpp
+++ Bashar and the bad land.cpp
@@ -24,16 +24,8 @@ void solve(){
 		// cout << l << ' ' << r << " " << t << "\n";
 		if(t >= x){
 			b = min(b, r - l + 1);
-			if(l == r){
-				t-=a[l];
-				l++;
-				r++;
-				t+=a[r];
-			}
-			else{
-				t-=a[l];
-				l++;
-			}
+			t-=a[l];
+			l++;
 			continue;
 		}
 		r++;
